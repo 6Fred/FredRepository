0232dc479464724d4a1a41d19841164a6e151679
Fred R <fredr3733@gmail.com>
2024-08-09 19:34:08 -0400

    fixed my files

--- Problems/Calculator/Program.cs
+++ Problems/Calculator/Program.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Globalization;
-
-namespace MyApplication
+﻿namespace Calculator
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("enter a number!");
             int numOne = Convert.ToInt32(Console.ReadLine());
-           
+
             Console.WriteLine("enter a second number!");
             int numTwo = Convert.ToInt32(Console.ReadLine());
-           
+
             Console.WriteLine(numTwo + numOne);
             Console.Read();
 
-        } 
+        }
     }
 }
-
-
