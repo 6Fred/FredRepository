1973bd7db344180dba5ec817d23ea0f34333766e
Fred R <fredr3733@gmail.com>
2024-08-09 18:53:06 -0400

    addition calculator

---
+++ Problems/Calc/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number!");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine("enter a second number!");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine(numTwo + numOne);
+            Console.Read();
+
+        } 
+    }
+}
+
+
