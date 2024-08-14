a0f941ba5710901dda90bfbb2c4e48293f50646b
Fred R <fredr3733@gmail.com>
2024-08-14 18:38:06 -0400

    added sumdigits

--- Problems/SumDigits/Program.cs
+++ Problems/SumDigits/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("dev branch!");
+            Console.WriteLine("List digits for the sum of them! ex. 1234 = 10");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            while (userInput != 0)
+            {
+                sum += userInput % 10;
+                userInput /= 10;
+            }
+            Console.WriteLine("sum of digits" + sum);
+            Console.Read();
+
+
+
+
         }
     }
 }
