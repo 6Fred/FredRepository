5b90b6d062ae2ccb1cc0cb095633f82507687b82
Fred R <fredr3733@gmail.com>
2024-08-14 16:53:06 -0400

    updated calc in main

--- Problems/Calculator/Program.cs
+++ Problems/Calculator/Program.cs
@@ -4,7 +4,75 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("add, subtract, divide, multiply");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "add")
+            {
+                AddMethod();
+            }
+            else if (userInput == "subtract")
+            {
+                SubMethod();
+            }
+            else if (userInput == "divide")
+            {
+                DivideMethod();
+            }
+            else if (userInput == "multiply")
+            {
+                MultiplyMethod();
+            }
+            else 
+            {
+                Console.WriteLine("choose one of those options!");
+                
+            }
+            Console.Read();
+        }
+        static void AddMethod()
+        {
+            Console.WriteLine("enter a number!");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter a second number!");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("your number is " + (numOne + numTwo) + "!");
+            Console.Read();
+        }
+        static void SubMethod()
+        {
+            Console.WriteLine("enter a number!");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter a second number!");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("your number is " + (numOne - numTwo) + "!");
+            Console.Read();
+        }
+        static void DivideMethod()
+        {
+            Console.WriteLine("enter a number!");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter a second number!");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("your number is " + (numOne / numTwo) + "!");
+            Console.Read();
+        }
+        static void MultiplyMethod()
+        {
+            Console.WriteLine("enter a number!");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter a second number!");
+            int numTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("your number is " + (numOne * numTwo) + "!");
+            Console.Read();
         }
     }
 }
