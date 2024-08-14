029d1edf39cf8b2b222ba39d5d980ffcdeba2237
Fred R <fredr3733@gmail.com>
2024-08-14 16:43:49 -0400

    removed calc from oddevendev

--- Problems/Calculator/Program.cs
+++ Problems/Calculator/Program.cs
@@ -4,14 +4,6 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter a number!");
-            int numOne = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("enter a second number!");
-            int numTwo = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine(numTwo + numOne);
-            Console.Read();
 
         }
     }
