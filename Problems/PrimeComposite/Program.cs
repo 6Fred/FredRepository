cdc81bc99050cd064f01dbc9358bc0fba73df02f
Fred R <fredr3733@gmail.com>
2024-08-14 18:23:26 -0400

    finished prime composite code (not mine)

--- Problems/PrimeComposite/Program.cs
+++ Problems/PrimeComposite/Program.cs
@@ -4,12 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter a number");
-            int userInput = Convert.ToInt32(Console.ReadLine());
-
-
-
-            Console.Read();
+            Console.WriteLine("type a number to see if its prime!");
+            int uInput = Convert.ToInt32(Console.ReadLine());
+            if (uInput == 0 || uInput == 1)
+            {
+                Console.WriteLine(uInput + " is not a prime number!");
+                Console.ReadLine();
+            }
+            else
+            {
+                for (int i = 2; i <= uInput/2; i++)
+                {
+                    if (uInput % i == 0)
+                    {
+                        Console.WriteLine(uInput + " is not a prime number");
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+                Console.WriteLine(uInput + " is a prime number!");
+                Console.ReadLine();
+            }
         }
     }
 }
