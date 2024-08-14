28c7e5b180e1e7cbee80632834a6fb0b38bea12e
Fred R <fredr3733@gmail.com>
2024-08-14 16:18:51 -0400

    added primecomposite

---
+++ Problems/PrimeComposite/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PrimeComposite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.Read();
+        }
+    }
+}
