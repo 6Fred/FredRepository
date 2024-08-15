2d63bfe3dc7d6f62832412f23cc211b2c97c7f02
Fred R <fredr3733@gmail.com>
2024-08-15 17:24:18 -0400

    added testing dev to test random stuff

---
+++ Problems/Testing/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please type in add, sub, div, mult");
+            string userInput = Console.ReadLine();
+            switch (userInput)
+            {
+                test test test
+
+
+
+            }
+        }
+    }
+}
