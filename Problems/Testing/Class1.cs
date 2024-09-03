6d8537672eeed9981847b935b1b5f41c032e7c40
Fred R <fredr3733@gmail.com>
2024-09-03 17:10:54 -0400

    na

--- Problems/Testing/Class1.cs
+++ Problems/Testing/Class1.cs
@@ -1,77 +1,15 @@
-﻿namespace Calculator
+﻿namespace Tester
 {
-     class Program
+    
+
+    class Program
     {
         static void Main(string[] args)
         {
-            CalcMethod myObj = new CalcMethod();
-            Console.WriteLine(CalcMethod.displayTest);
-            string[] validCommands = { "add", "subtract", "divide", "multiply" };
-            string userInput = "";
-
-            while (true)
-            {
-                Console.WriteLine("please enter add, subtract, divide or multiply!");
-                userInput = Console.ReadLine()?.Trim().ToLower();
-
-                if (Array.Exists(validCommands, x => x == userInput))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("type on of those options silly! ");
-                }
-            }
-
-            int numOne = EnterNumber("first number!");
-            int numTwo = EnterNumber("second number!");
-
-            switch (userInput)
-            {
-                case "add":
-                    AddMethod(numOne, numTwo);
-                    break;
-                case "subtract":
-                    SubMethod(numOne, numTwo);
-                    break;
-                case "divide":
-                    DivideMethod(numOne, numTwo);
-                    break;
-                case "multiply":
-                    MultiplyMethod(numOne, numTwo);
-                    break;
-                default:
-                    Console.WriteLine("choose one of those options!");
-                    break;
-            }
-        }
-        static void AddMethod(int numOne, int numTwo)
-        {
-            Console.WriteLine("your number is " + (numOne + numTwo) + "!");
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            Console.WriteLine(myObj.Name);
             Console.Read();
-        }
-        static void SubMethod(int numOne, int numTwo)
-        {
-            Console.WriteLine("your number is " + (numOne - numTwo) + "!");
-            Console.Read();
-        }
-        static void DivideMethod(int numOne, int numTwo)
-        {
-            Console.WriteLine("your number is " + (numOne / numTwo) + "!");
-            Console.Read();
-        }
-        static void MultiplyMethod(int numOne, int numTwo)
-        {
-            Console.WriteLine("your number is " + (numOne * numTwo) + "!");
-            Console.Read();
-        }
-        static int EnterNumber(string numberSequence)
-        {
-            Console.WriteLine("Enter your " + numberSequence);
-            int num = Convert.ToInt32(Console.ReadLine());
-            return num;
-
         }
     }
 
---
+++ Problems/Testing/Class2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+        class Person
+    {
+        private string name; // field
+        public string Name   // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}
+}
