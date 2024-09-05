d366b23053b4e5644e8887adb0678909653e372a
Fred R <fredr3733@gmail.com>
2024-09-05 17:50:01 -0400

    na

--- Problems/Calculator/Program.cs
+++ Problems/Calculator/Program.cs
@@ -5,35 +5,45 @@
         static void Main(string[] args)
         {
             string[] validCommands = { "add", "subtract", "divide", "multiply" };
-            string userInput = Methods.TestMethod(validCommands);
-            
-            int numOne = Methods.EnterNumber("first number!");
-            int numTwo = Methods.EnterNumber("second number!");
+            bool keepRunning = true;
 
-            switch (userInput)
+            while (keepRunning)
             {
-                case "add":
-                    Methods.AddMethod(numOne, numTwo);
-                    break;
-                case "subtract":
-                    Methods.SubMethod(numOne, numTwo);
-                    break;
-                case "divide":
-                    Methods.DivideMethod(numOne, numTwo);
-                    break;
-                case "multiply":
-                    Methods.MultiplyMethod(numOne, numTwo);
-                    break;
-                default:
-                    Console.WriteLine("choose one of those options!");
-                    break;
+                string userInput = Methods.TestMethod(validCommands);
+
+                if (userInput == "exit")
+                {
+                    keepRunning = false;
+                    continue;
+                }
+                int numOne = Methods.EnterNumber("first number!");
+                int numTwo = Methods.EnterNumber("second number!");
+
+                switch (userInput)
+                {
+                    case "add":
+                        Methods.AddMethod(numOne, numTwo);
+                        break;
+                    case "subtract":
+                        Methods.SubMethod(numOne, numTwo);
+                        break;
+                    case "divide":
+                        Methods.DivideMethod(numOne, numTwo);
+                        break;
+                    case "multiply":
+                        Methods.MultiplyMethod(numOne, numTwo);
+                        break;
+                    default:
+                        Console.WriteLine("choose one of those options!");
+                        break;
+                }
+
             }
 
+
+
+
+
         }
-        
-        
-
-
-
     }
 }
