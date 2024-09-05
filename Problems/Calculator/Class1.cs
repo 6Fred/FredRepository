1b007673a0332c1d8539f7b5ad14d920547975c8
Fred R <fredr3733@gmail.com>
2024-09-05 17:29:17 -0400

    attempting to make a seperate class for methods to clean up code

---
+++ Problems/Calculator/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace Calculator
+{
+    public static class Methods
+    {
+        public static void AddMethod(int numOne, int numTwo)
+        {
+            Console.WriteLine("your number is " + (numOne + numTwo) + "!");
+            Console.Read();
+        }
+        public static void SubMethod(int numOne, int numTwo)
+        {
+            Console.WriteLine("your number is " + (numOne - numTwo) + "!");
+            Console.Read();
+        }
+        public static void DivideMethod(int numOne, int numTwo)
+        {
+            Console.WriteLine("your number is " + (numOne / numTwo) + "!");
+            Console.Read();
+        }
+        public static void MultiplyMethod(int numOne, int numTwo)
+        {
+            Console.WriteLine("your number is " + (numOne * numTwo) + "!");
+            Console.Read();
+        }
+        public static int EnterNumber(string numberSequence)
+        {
+            Console.WriteLine("Enter your " + numberSequence);
+            int num = Convert.ToInt32(Console.ReadLine());
+            return num;
+        }
+        public static string TestMethod(string[] validCommands)
+        {
+            while (true)
+            {
+                Console.WriteLine("please enter add, subtract, divide or multiply!");
+                string userInput = Console.ReadLine()?.Trim().ToLower();
+
+                if (Array.Exists(validCommands, x => x == userInput))
+                {
+                    return userInput;
+
+                }
+                else
+                {
+                    Console.WriteLine("type on of those options silly! ");
+                }
+            }
+        }
+    }
+}
--- Problems/Calculator/Program.cs
+++ Problems/Calculator/Program.cs
@@ -1,23 +1,23 @@
-﻿namespace Calculator
+﻿
+
+namespace Calculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string[] validCommands = { "add", "subtract", "divide", "multiply" };
-            string userInput = "";
+            string userInput = TestMethod(validCommands);
             
-            TestMethod(userInput, validCommands);
 
-
-            sint numOne = EnterNumber("first number!");
+            int numOne = EnterNumber("first number!");
             int numTwo = EnterNumber("second number!");
 
 
             switch (userInput)
             {
                 case "add":
-                    AddMethod(numOne, numTwo);
+                    Methods.AddMethod(numOne, numTwo);
                     break;
                 case "subtract":
                     SubMethod(numOne, numTwo);
@@ -34,49 +34,7 @@
             }
 
         }
-        static void AddMethod(int numOne, int numTwo)
-        {
-            Console.WriteLine("your number is " + (numOne + numTwo) + "!");
-            Console.Read();
-        }
-        static void SubMethod(int numOne, int numTwo)
-        {
-            Console.WriteLine("your number is " + (numOne - numTwo) + "!");
-            Console.Read();
-        }
-        static void DivideMethod(int numOne, int numTwo)
-        {
-            Console.WriteLine("your number is " + (numOne / numTwo) + "!");
-            Console.Read();
-        }
-        static void MultiplyMethod(int numOne, int numTwo)
-        {
-            Console.WriteLine("your number is " + (numOne * numTwo) + "!");
-            Console.Read();
-        }
-        static int EnterNumber(string numberSequence)
-        {
-            Console.WriteLine("Enter your " + numberSequence);
-            int num = Convert.ToInt32(Console.ReadLine());
-            return num;
-        }
-        static void TestMethod(string userInput, string[] validCommands)
-        {
-            while (true)
-            {
-                Console.WriteLine("please enter add, subtract, divide or multiply!");
-                userInput = Console.ReadLine()?.Trim().ToLower();
-
-                if (Array.Exists(validCommands, x => x == userInput))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("type on of those options silly! ");
-                }
-            }
-        }
+        
         
 
 
