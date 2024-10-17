440864860ff8ec6239d4dec5f62aed99874d27cf
Fred R <fredr3733@gmail.com>
2024-10-16 23:33:13 -0400

    added bunch of fun stuff, completely reworked!

--- Problems/SumDigits/Program.cs
+++ Problems/SumDigits/Program.cs
@@ -20,12 +20,12 @@ namespace SumDigits
                     continue;
                 }
             } 
-            
-            
         }
+            
 
-
-            static void SumMethod(int userInput, int totalSum)
+        
+        
+        static void SumMethod(int userInput, int totalSum)
         {
             while (userInput != 0)
             {
@@ -33,7 +33,12 @@ namespace SumDigits
                 userInput /= 10;
             }
             Console.WriteLine("the sum is " + totalSum);
-            Console.Read();
+            Thread.Sleep(3000);
+            Console.WriteLine("i hope you're happy.");
+            Thread.Sleep(3000);
+            Console.WriteLine("bye");
+            Thread.Sleep(1500);
+            Environment.Exit(0);
         }
             static string GreetingMethod(string[] validWords, int totalSum)
         {
@@ -41,27 +46,50 @@ namespace SumDigits
             {
                 Console.WriteLine("List digits for their total sum! ex. 1234 = 10");
                 int userInput = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Wow! " + userInput + " is my favorite number! Are you sure you want to use this number?");
-                string uInput = Console.ReadLine()?.Trim().ToLower();
-                
+                Thread.Sleep(1000);
+                Console.WriteLine("Wow! " + userInput + " is my favorite number!");
+                Thread.Sleep(1750);
+                Console.WriteLine("Are you sure you want to use this number? ");
+
+
+                string uInput;
+
+
+                while (true)
+                {
+                    uInput = Console.ReadLine()?.Trim().ToLower();
+
+                    if (Array.Exists(validWords, x => x ==uInput))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("SAY YES OR NO!!!");
+                    }
+                }
                 if (uInput == "no")
                 {
+                    Thread.Sleep(2000);
+                    Console.WriteLine("really?");
+                    Thread.Sleep(1800);
+                    Console.WriteLine("wow.");
+                    Thread.Sleep(1750);
+                    Console.WriteLine("just wow.");
+                    Thread.Sleep(1750);
                     Console.WriteLine("lame. goodbye");
-                    Thread.Sleep(5000);
+                    Thread.Sleep(1750);
                     Environment.Exit(0);
                 }
+                Thread.Sleep(500);
+                Console.WriteLine("calculating...");
+                Thread.Sleep(1000);
+                Console.WriteLine("almost there...");
+                Thread.Sleep(2000);
+                SumMethod(userInput, totalSum);
 
-
-                //Console.WriteLine(uResult);
-
-                if (Array.Exists(validWords, x => x == uInput))
-                {
-                    SumMethod(userInput, totalSum);
-                }
-                else
-                {
-                    Console.WriteLine("SAY YES OR NO!!!");
-                }
+                return uInput;
+               
             }   
         }
 
