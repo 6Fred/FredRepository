1332372e7846648ea7d11c8bfcb70c442bb81d23
Fred R <fredr3733@gmail.com>
2024-10-20 18:36:26 -0400

    made palindrome, big help from chatgpt. i want to learn it more and understand it better.

--- Problems/Palindrome/Program.cs
+++ Problems/Palindrome/Program.cs
@@ -4,8 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("help");
+            Console.WriteLine("type a word to check if its a palindrome");
+            string userInput = Console.ReadLine();
+
+            if (IsPalindrome(userInput))
+            {
+                Console.WriteLine($"{userInput} is a palindrome.");
+                Console.Read();
+            }
+            else
+            {
+                Console.WriteLine($"{userInput} is NOT a palindrome.");
+                Console.Read();
+            }
+        }
+        static bool IsPalindrome(string word)
+        {
+            word = word.ToLower();
+            word = word.Replace(" ", "");
+
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);  
+            string reversedWord = new string(charArray);
+
+            return word == reversedWord;
+
+
         }
     }
 }
